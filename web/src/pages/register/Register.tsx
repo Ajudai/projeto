b1db55bdc0dd9a2f73e25202d803857af2505a93
Raul Henrique <raulheri43@hotmail.com>
2023-06-01 19:09:38 +0000

    pagina de registro, chakra-ui, axios

---
+++ web/src/pages/register/Register.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const Register = () => {
+    const [nome, setNome] = useState<string>();
+    
+    useEffect(() => {}, []);
+    
+    return (
+        <main>
+            <h1>Registre-se</h1>
+        </main>
+    )
+}
+
+export default Register;
