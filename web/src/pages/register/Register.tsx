2d7ba8079a971a7de20040d53a35070155293318
Raul Henrique <raulheri43@hotmail.com>
2023-06-01 19:09:57 +0000

    styles register

--- web/src/pages/register/Register.tsx
+++ web/src/pages/register/Register.tsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import styles from './register.module.scss'
 
 const Register = () => {
     const [nome, setNome] = useState<string>();
     
     useEffect(() => {}, []);
-    
+
     return (
         <main>
             <h1>Registre-se</h1>
