743d76b4622c008e9eefa84a09ad8cb652637ef4
Raul Henrique <raulheri43@hotmail.com>
2023-06-02 11:51:00 +0000

    functional component na home

---
+++ web/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import styles from './home.module.scss';
+
+const Home = () => {
+    return (
+        <main className={styles.mainHome}>
+            <h1>HOME</h1>
+        </main>
+    )
+}
+
+export default Home;
