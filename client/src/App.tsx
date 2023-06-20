9362c887414c3b05f0a78def5efd1cef0ec46c31
DESKTOP-7M01SED\Aluno <aluno@jaboatao.ifpe.edu.br>
2023-06-20 19:50:40 -0300

    screen/register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 import ForgotPassword from './pages/token/ForgotPassword';
 
 function App() {
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,26 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import styles from './register.module.scss';
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import facebookMedia from "../../assets/facebookMedia.svg";
 import googleMedia from "../../assets/googleMedia.svg";
+import logo from "../../assets/logo.svg";
 
 const Register = () => {
     const [nome, setNome] = useState<string>();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const register = async () => {
+        await axios.post('http://localhost:5000/register', {
+          userName: nome,
+          userEmail: email,
+          userPassword: password
+        })
+          .then((res) => {
+            localStorage.setItem("userData", JSON.stringify(res?.data));
+            console.log(res.data)
+          })
+          .catch((error) => {
+            console.error("Erro ao cadastrar", error.response.data.message)
+          })
+      }
+
     return (
         <main className={styles.registerPageMain}>
             <div className={styles.registerPageImg}>
                 <h1 className={styles.registerPageH1}>Recife registra alta poluição no Rio Capibaribe</h1>
-                <h2 className={styles.registerPageH2}>Fundação Recife</h2>
             </div>
+            <img src={logo} alt="Ajudaí logo" className={styles.PageLogoImg} />
             <div>
                 <Input label="Nome" onChange={(e) => setNome(e.target.value)} />
+                <Input label="Telefone" onChange={(e) => setPassword(e.target.value)}/>
                 <Input label="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <Input label="Senha" onChange={(e) => setPassword(e.target.value)}/>
+                <Input label="Senha" onChange={(e) => setPassword(e.target.value)}/>
                 <div className={styles.registerCheckbox}>
                 <input type="checkbox" id="registerCheckbox"></input>
                 <label htmlFor="registerCheckbox">Concordo com os termos de uso</label>
@@ -30,9 +47,7 @@ const Register = () => {
                     size="medium"
                     disabled
                     rounded
-                    onClick={() => {
-                        console.log("Registrado");
-                    }}
+                    onClick={register}
                     label="Continuar"
                     />
                 </div>
