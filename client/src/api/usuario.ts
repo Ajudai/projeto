35c1ebf613cec22b2adecc84803f3a05e0f4f361
Your Name <raulheri43@hotmail.com>
2023-07-07 21:09:45 -0300

    fix

--- client/src/api/usuario.ts
+++ client/src/api/usuario.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { api } from '../service/api';
 
 interface IUserDataUpdate {
@@ -6,6 +7,29 @@ interface IUserDataUpdate {
   telefone: string;
   _id: string;
 }
+
+interface IUserLogin {
+  userEmail: string;
+  userPassword: string;
+}
+
+export const login = async ({ userEmail, userPassword }: IUserLogin) => {
+  try {
+    const res = await api
+      .post<IUserDataUpdate>(`login`, {
+        userEmail,
+        userPassword,
+      })
+      .then(() => {
+        localStorage.setItem('userData', JSON.stringify(res?.data));
+      });
+    return { data: res.data };
+  } catch (error: any) {
+    if (error.response.data.message) {
+      return { error: error.response.data.message };
+    }
+  }
+};
 
 export const editarUsuario = async ({ nome, email, telefone, _id }: IUserDataUpdate) => {
   try {
@@ -19,6 +43,5 @@ export const editarUsuario = async ({ nome, email, telefone, _id }: IUserDataUpd
     if (error.response.data.message) {
       return { error: error.response.data.message };
     }
-    return { error: 'Erro desconhecido' };
   }
 };
