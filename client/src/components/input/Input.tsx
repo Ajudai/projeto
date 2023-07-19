b9e8f79cd48ec12bcabdfb8731eef39e20e3e58c
Ana St <anna1100st@gmail.com>
2023-07-19 12:18:12 -0300

    Ajuste no Input

--- client/src/components/input/Input.tsx
+++ client/src/components/input/Input.tsx
@@ -9,14 +9,15 @@ interface IInput {
   type: HTMLInputTypeAttribute;
   value: string;
   name?: string;
+  placeholder?: string;
 }
 
-const Input: React.FC<IInput> = ({ label, onChange, error, requestHelp, type, value, name }) => {
+const Input: React.FC<IInput> = ({ label, onChange, error, requestHelp, type, value, name, placeholder }) => {
   const inputClasses = `${styles.inputComponentInput} ${requestHelp ? styles.requestHelp : ''}`;
   return (
     <div className={styles.inputComponentDiv}>
       <p className={error ? styles.inputComponentLabelError : styles.inputComponentLabel}>{label}</p>
-      <input value={value} name={name} className={inputClasses} onChange={onChange} type={type} />
+      <input value={value} name={name} className={inputClasses} onChange={onChange} type={type} placeholder={placeholder}/>
     </div>
   );
 };
